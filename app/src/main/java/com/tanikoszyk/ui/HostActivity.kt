06c7ae8279ff5d6613830ee565b8902481cb4512
Artyom Vlasov <artyom.vlasov@droidsonroids.pl>
2019-04-21 12:08:33 +0200

    Implement tab changing animation

--- app/src/main/java/com/tanikoszyk/ui/HostActivity.kt
+++ app/src/main/java/com/tanikoszyk/ui/HostActivity.kt
@@ -1,17 +1,16 @@
 package com.tanikoszyk.ui
 
-import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.transaction
 import com.tanikoszyk.R
 import com.tanikoszyk.ui.auth.launchAuthenticationActivity
-import com.tanikoszyk.ui.base.BaseActivity
 import kotlinx.android.synthetic.main.activity_host.*
 
-fun Context.launchHostActivity(){
+fun Context.launchHostActivity() {
     val intent = Intent(this, HostActivity::class.java).apply {
         flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
     }
@@ -31,29 +30,41 @@ class HostActivity : AppCompatActivity() {
         navigationBar.setOnNavigationItemSelectedListener {
             when (it.itemId) {
                 R.id.navigationHome -> {
-                    homeFragmentContainer.visibility = View.VISIBLE
-                    cartFragmentContainer.visibility = View.GONE
-                    profileFragmentContainer.visibility = View.GONE
+                    homeNavHostFragment.show()
+                    cartNavHostFragment.hide()
+                    profileNavHostFragment.hide()
                     true
                 }
                 R.id.navigationCart -> {
-                    homeFragmentContainer.visibility = View.GONE
-                    cartFragmentContainer.visibility = View.VISIBLE
-                    profileFragmentContainer.visibility = View.GONE
+                    homeNavHostFragment.hide()
+                    cartNavHostFragment.show()
+                    profileNavHostFragment.hide()
                     true
                 }
                 R.id.navigationProfile -> {
-                    homeFragmentContainer.visibility = View.GONE
-                    cartFragmentContainer.visibility = View.GONE
-                    profileFragmentContainer.visibility = View.VISIBLE
+                    homeNavHostFragment.hide()
+                    cartNavHostFragment.hide()
+                    profileNavHostFragment.show()
                     true
                 }
                 else -> false
             }
         }
+        navigationBar.selectedItemId = R.id.navigationHome
     }
 
-    fun navigateToAuthenticationScreen(){
+    fun navigateToAuthenticationScreen() {
         launchAuthenticationActivity()
+    }
+
+    private fun Fragment.show() {
+        requireFragmentManager().transaction {
+            setCustomAnimations(R.anim.animation_show_fragment, R.anim.animation_hide_fragment)
+            show(this@show)
+        }
+    }
+
+    private fun Fragment.hide() {
+        requireFragmentManager().transaction { hide(this@hide) }
     }
 }
